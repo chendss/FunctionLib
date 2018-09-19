b3be6547bd9544c7fcb12a95835b36b57fec0092
dashao <dashao@work.com>
2018-09-19 14:35:54 +0800

    作业迁移

--- typescript/extends.js
+++ typescript/extends.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var DebugPro_1 = require("./DebugPro");
 var ObjcectPro_1 = require("./ObjcectPro");
-var rest = ObjcectPro_1.chainObject(["fuckf", "fuck1"], 11);
+var rest = ObjcectPro_1.chainObject(["fuck", "fuck1"], 11);
 DebugPro_1.log(rest);
 //# sourceMappingURL=extends.js.map
