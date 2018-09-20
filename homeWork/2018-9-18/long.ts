d8320fd91d97529422e6c5bc036af5cd83be071d
long <long@long.long>
2018-09-20 21:18:00 +0800

    修正

--- homeWork/2018-9-18/long.ts
+++ homeWork/2018-9-18/long.ts
@@ -26,13 +26,13 @@ const anyToArray = function (val: any): Array<any> {
 }
 
 const isEqual = function (a, b): boolean {
-    if (a instanceof Object) {
-        if (b instanceof Object) {
-            if (Object.keys(a).length !== Object.keys(b).length) {
+    if (a instanceof Array) {
+        if (b instanceof Array) {
+            if (a.length !== b.length) {
                 return false
             } else {
-                for (let key of Object.keys(a)) {
-                    if (!isEqual(a[key], b[key])) {
+                for (let index in a) {
+                    if (!isEqual(a[index], b[index])) {
                         return false
                     }
                 }
@@ -41,13 +41,13 @@ const isEqual = function (a, b): boolean {
         } else {
             return false
         }
-    } else if (a instanceof Array) {
-        if (b instanceof Array) {
-            if (a.length !== b.length) {
+    } else if (a instanceof Object) {
+        if (b instanceof Object) {
+            if (Object.keys(a).length !== Object.keys(b).length) {
                 return false
             } else {
-                for (let index in a) {
-                    if (!isEqual(a[index], b[index])) {
+                for (let key of Object.keys(a)) {
+                    if (!isEqual(a[key], b[key])) {
                         return false
                     }
                 }
@@ -69,7 +69,7 @@ const main = function (): boolean {
     for (let i = 0; i < input.length; i++) {
         let target_item = target[i]
         let input_item = input[i]
-        if (isEqual(target_item,anyToArray( input_item)) === false) {
+        if (isEqual(target_item, anyToArray(input_item)) === false) {
             return false
         }
     }
